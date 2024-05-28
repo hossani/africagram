791f1d8cfb867405233366cc037095075b777cb1
hossani <hamza.hossani.001@gmail.com>
2024-05-28 21:01:04 +0100

    third

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,9 @@
 const express=require('express');
-const route=express.router();
+const route=express.Router();
 
 const {login,register}=require('../controllers/AuthController');
 
 route.post('/register',register);
 route.post('/login',login);
+
+module.exports=route;
