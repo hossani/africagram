86d18020f23f7a6ce4c4ee5338542a936765482c
hossani <hamza.hossani.001@gmail.com>
2024-05-30 16:00:15 +0100

    changement now

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ require("dotenv").config()
 const app=express();
 const authRoute=require('./routes/AuthRoutes');
 const posteRoute=require('./routes/PostRoutes');
+const likesRoute=require('./routes/LikesRoutes');
 
 app.use(express.json());
 app.use(authRoute);
 app.use(posteRoute);
+app.use(likesRoute);
 
 const port=process.env.APP_PORT || 8000
 app.listen(port,()=>{
